0bf7f98b6360f2bfc57c98130dd971954666f941
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-22 22:38:50 +0100

    new exception type for connection string issues

---
+++ sources/IoT.Simulator/IoT.Simulator/Exceptions/ConnectionStringException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IoT.Simulator.Exceptions
+{
+    public class ConnectionStringException : ArgumentException
+    {
+        public ConnectionStringException(string message) : base(message) { }
+
+        public ConnectionStringException(string message, string paramName) : base(message, paramName) { }
+    }
+}
