40bd27431026dc9a5f77329503f40c7baae55b26
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-22 22:55:45 +0100

    DPS parameters

---
+++ sources/IoT.Simulator/IoT.Simulator/Settings/DPS/EnrollmentType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Simulator.Settings.DPS
+{
+    /// <summary>
+    /// The type of enrollment for a device in the provisioning service.
+    /// </summary>
+    public enum EnrollmentType
+    {
+        /// <summary>
+        ///  Enrollment for a single device.
+        /// </summary>
+        Individual,
+
+        /// <summary>
+        /// Enrollment for a group of devices.
+        /// </summary>
+        Group,
+    }
+}
