4e17c1a229f2596b09c2ec6336ee4e1789c4c8b2
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-26 19:28:47 +0100

    Settings serialization and persistence methods

--- sources/IoT.Simulator/IoT.Simulator/Tools/ConfigurationHelpers.cs
+++ sources/IoT.Simulator/IoT.Simulator/Tools/ConfigurationHelpers.cs
@@ -1,9 +1,11 @@
 ﻿using IoT.Simulator.Exceptions;
 
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 using System;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +15,7 @@ namespace IoT.Simulator.Tools
     {
         public static readonly string APP_SETTINGS_FILE_NAME = "appsettings.json";
         public static readonly string DEVICE_SETTINGS_FILE_NAME = "devicesettings.json";
-        public static readonly string MODULE_SETTINGS_FILE_NAME = "modulessettings.json";
+        public static readonly string MODULES_SETTINGS_FILE_NAME = "modulessettings.json";
         public static readonly string DPS_SETTINGS_FILE_NAME = "dpssettings.json";
 
         public static void CheckEnvironmentConfigurationFiles()
@@ -27,8 +29,8 @@ namespace IoT.Simulator.Tools
             if (!File.Exists(DEVICE_SETTINGS_FILE_NAME))
                 sb.AppendLine($"{DEVICE_SETTINGS_FILE_NAME} not found.");
 
-            if (!File.Exists(MODULE_SETTINGS_FILE_NAME))
-                sb.AppendLine($"{MODULE_SETTINGS_FILE_NAME} not found.");
+            if (!File.Exists(MODULES_SETTINGS_FILE_NAME))
+                sb.AppendLine($"{MODULES_SETTINGS_FILE_NAME} not found.");
 
             if (sb.Length > 0)
                 throw new MissingEnvironmentConfigurationFileException(sb.ToString());
@@ -45,11 +47,11 @@ namespace IoT.Simulator.Tools
             if (!File.Exists(appsettings))
                 sb.AppendLine($"{appsettings} not found.");
 
-            var devicesettings = $"{APP_SETTINGS_FILE_NAME}.{environment}.json";
+            var devicesettings = $"{DEVICE_SETTINGS_FILE_NAME}.{environment}.json";
             if (!File.Exists(devicesettings))
                 sb.AppendLine($"{devicesettings} not found.");
 
-            var modulessettings = $"{APP_SETTINGS_FILE_NAME}.{environment}.json";
+            var modulessettings = $"{MODULES_SETTINGS_FILE_NAME}.{environment}.json";
             if (!File.Exists(modulessettings))
                 sb.AppendLine($"{modulessettings} not found.");
 
@@ -61,10 +63,31 @@ namespace IoT.Simulator.Tools
 
         public static async Task WriteDeviceSettings(JToken content)
         {
+            await WriteSettings(content, DEVICE_SETTINGS_FILE_NAME);
+        }
+
+        public static async Task WriteModulesSettings(JToken content)
+        {
+            await WriteSettings(content, MODULES_SETTINGS_FILE_NAME);
+        }
+
+        public static async Task WriteDpsSettings(JToken content)
+        {
+            await WriteSettings(content, DPS_SETTINGS_FILE_NAME);
+        }
+
+
+        private static async Task WriteSettings(JToken content, string fileName)
+        {
             if (content == null)
                 throw new ArgumentNullException(nameof(content));
 
-            //Store the content at the application root
+            if (string.IsNullOrEmpty(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+
+            await File.WriteAllTextAsync(
+                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), fileName),
+                JsonConvert.SerializeObject(content, Formatting.Indented));            
         }
 
     }
