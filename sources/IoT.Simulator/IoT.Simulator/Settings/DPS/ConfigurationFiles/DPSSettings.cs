079f069113233a256107a4b9a63db90493c77fea
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-26 17:13:04 +0100

    DPS settings entity for json deserialization

--- sources/IoT.Simulator/IoT.Simulator/Settings/DPS/ConfigurationFiles/DPSSettings.cs
+++ sources/IoT.Simulator/IoT.Simulator/Settings/DPS/ConfigurationFiles/DPSSettings.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,25 @@ namespace IoT.Simulator.Settings.DPS
 {
     public class DPSSettings
     {
+        [JsonProperty("individualEnrollment")]
+        public IndividualEnrollment IndividualEnrollment {get;set;}
+        [JsonProperty("groupEnrollment")]
+        public GroupEnrollment GroupEnrollment { get; set; }
+
+    }
+
+    public class IndividualEnrollment
+    {
+
+    }
+
+    public class GroupEnrollment
+    {
+        [JsonProperty("symetricKeySettings")]
         public DPSSymmetricKeySettings SymetricKeySettings { get; set; }
+        [JsonProperty("X509Settings")]
         public DPSX509Settings X509Settings { get; set; }
+        [JsonProperty("tpmSettings")]
         public DPSTPMSettings TMPSettings { get; set; }
     }
 }
