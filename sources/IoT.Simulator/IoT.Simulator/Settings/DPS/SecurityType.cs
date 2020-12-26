9e6b32b4afbf4aa6ae0fbc8d5ba9498d03431082
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-26 18:51:39 +0100

    security type for provisioning

---
+++ sources/IoT.Simulator/IoT.Simulator/Settings/DPS/SecurityType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.Simulator.Settings.DPS
+{
+    /// <summary>
+    /// The type of enrollment for a device in the provisioning service.
+    /// </summary>
+    public enum SecurityType
+    {
+        SymetricKey,
+        X509,
+        TPM
+    }
+}
