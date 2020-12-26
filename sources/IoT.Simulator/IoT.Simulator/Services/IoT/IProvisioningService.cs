a41714487f31026bb2f3865ee7e6fe5ea1850724
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-26 18:50:03 +0100

    new provisioning interface

---
+++ sources/IoT.Simulator/IoT.Simulator/Services/IoT/IProvisioningService.cs
@@ -0,0 +1,11 @@
+﻿using IoT.Simulator.Settings.DPS;
+
+using System.Threading.Tasks;
+
+namespace IoT.Simulator.Services
+{
+    interface IProvisioningService
+    {
+        Task<string> ProvisionDevice();
+    }
+}
