068356028e192a4dc24d3012f460c5b719a5b1c9
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2020-12-26 21:43:19 +0100

    error fix

--- sources/IoT.Simulator/IoT.Simulator/Services/IoT/IProvisioningService.cs
+++ sources/IoT.Simulator/IoT.Simulator/Services/IoT/IProvisioningService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace IoT.Simulator.Services
 {
-    interface IProvisioningService
+    public interface IProvisioningService
     {
         Task<string> ProvisionDevice();
     }
