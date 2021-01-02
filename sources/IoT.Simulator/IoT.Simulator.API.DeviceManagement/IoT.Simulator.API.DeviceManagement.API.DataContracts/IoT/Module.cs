eaa7e8849e7bbd05fc4e4dde274144b26e14cfff
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-01-02 22:00:11 +0100

    mapping bug fix

--- sources/IoT.Simulator/IoT.Simulator.API.DeviceManagement/IoT.Simulator.API.DeviceManagement.API.DataContracts/IoT/Module.cs
+++ sources/IoT.Simulator/IoT.Simulator.API.DeviceManagement/IoT.Simulator.API.DeviceManagement.API.DataContracts/IoT/Module.cs
@@ -8,13 +8,13 @@ namespace IoT.Simulator.API.DeviceManagement.API.DataContracts.IoT
     public class Module
     {
         public string Id { get; set; }
-        public string DeviceId { get; }
-        public string GenerationId { get; }
+        public string DeviceId { get; set; }
+        public string GenerationId { get; set; }
         public string ETag { get; set; }
         public string ConnectionState { get; set; }      
         public string ManagedBy { get; set; }
         public DateTime ConnectionStateUpdatedTime { get; set; }        
-        public DateTime LastActivityTime { get; }
+        public DateTime LastActivityTime { get; set; }
         public int CloudToDeviceMessageCount { get; set; }
         public string AuthenticationMechanism { get; set; }
         public string AuthenticationPrimaryKey { get; set; }
--- sources/IoT.Simulator/IoT.Simulator.API.DeviceManagement/IoT.Simulator.API.DeviceManagement.Services/Model/IoT/Module.cs
+++ sources/IoT.Simulator/IoT.Simulator.API.DeviceManagement/IoT.Simulator.API.DeviceManagement.Services/Model/IoT/Module.cs
@@ -5,13 +5,13 @@ namespace IoT.Simulator.API.DeviceManagement.Services.Model.IoT
     public class Module
     {
         public string Id { get; set; }
-        public string DeviceId { get; }
-        public string GenerationId { get; }
+        public string DeviceId { get; set; }
+        public string GenerationId { get; set; }
         public string ETag { get; set; }
         public string ConnectionState { get; set; }      
         public string ManagedBy { get; set; }
         public DateTime ConnectionStateUpdatedTime { get; set; }        
-        public DateTime LastActivityTime { get; }
+        public DateTime LastActivityTime { get; set; }
         public int CloudToDeviceMessageCount { get; set; }
         public string AuthenticationType { get; set; }
         public string AuthenticationPrimaryKey { get; set; }
