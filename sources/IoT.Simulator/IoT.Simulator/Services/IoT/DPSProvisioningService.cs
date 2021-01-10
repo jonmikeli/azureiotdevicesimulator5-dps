c65fa45127b1b6fb2697374a13bcd88a6dc6957e
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-01-10 16:10:05 +0100

    code review

--- sources/IoT.Simulator/IoT.Simulator/Services/IoT/DPSProvisioningService.cs
+++ sources/IoT.Simulator/IoT.Simulator/Services/IoT/DPSProvisioningService.cs
@@ -201,11 +201,14 @@ namespace IoT.Simulator.Services
                                 _logger.LogWarning($"{logPrefix}::{_deviceSettingsDelegate.CurrentValue.ArtifactId}::Keys provided by the Device Management service.");
 
                                 if (!string.IsNullOrEmpty(primaryKey))
+                                {
+                                    _logger.LogDebug($"{logPrefix}::{_deviceSettingsDelegate.CurrentValue.ArtifactId}::{moduleId}::Module identity added to device.");
+
                                     result = $"HostName={_deviceSettingsDelegate.CurrentValue.HostName};DeviceId={_deviceSettingsDelegate.CurrentValue.DeviceId};ModuleId={moduleId};SharedAccessKey={primaryKey}";
+                                }
                                 else
                                     _logger.LogError($"{logPrefix}::{_deviceSettingsDelegate.CurrentValue.ArtifactId}::An error has occurred when getting the keys from the Device Management service.");
 
-                                _logger.LogDebug($"{logPrefix}::{_deviceSettingsDelegate.CurrentValue.ArtifactId}::{moduleId}::Module identity added to device.");
                             }
                         }
                     }
