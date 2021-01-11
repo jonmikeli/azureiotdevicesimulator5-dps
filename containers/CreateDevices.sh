caa0c4efd9274132e7bef4c085ff641567bf4c3d
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-01-11 23:17:39 +0100

    code cleaning

--- containers/CreateDevices.sh
+++ containers/CreateDevices.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "test"
-
 i=1
 
 while [ $i -lt 20 ]
@@ -10,5 +8,6 @@ do
   echo "Creating the device: $deviceName"
   docker run -ti --rm --name $deviceName -d -e PROVISIONING_REGISTRATION_ID=$deviceName -e ENVIRONMENT=Development --network="host" iotsimulator-dps
   echo "Device created: $deviceName"
+  
   let "i+=1" 
 done
