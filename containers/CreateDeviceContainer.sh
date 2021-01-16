982e7df3ec2f23e482a66c7f1fa13599b4c4dacd
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-01-16 22:23:16 +0100

    script updates

--- containers/CreateDeviceContainer.sh
+++ containers/CreateDeviceContainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "IoT Simulator DPS-Image-Creation"
-docker build -t iotsimulator-dps -f ..\sources\IoT.Simulator\IoT.Simulator\Dockerfile ..\sources\IoT.Simulator
+docker build -t iotsimulator-dps -f ..\\sources\\IoT.Simulator\\IoT.Simulator\\Dockerfile ..\\sources\\IoT.Simulator
 
 echo "IoT Simulator DPS-Container-Creation"
 docker run -ti --rm --name device10 -e PROVISIONING_REGISTRATION_ID=device10 -e ENVIRONMENT=Development --network="host" iotsimulator-dps
