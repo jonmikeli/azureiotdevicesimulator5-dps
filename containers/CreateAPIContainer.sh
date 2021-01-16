b9faab62cfce778cf16a7b0b09edce21433de2e5
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-01-16 22:21:24 +0100

    code comments and bash file

--- containers/CreateAPIContainer.sh
+++ containers/CreateAPIContainer.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 
 #1-Build the docker image
+echo "IoT HUB SDK REST API-Image-Creation"
 docker build -t iothubsdk-api -f ..\sources\IoT.Simulator\IoT.Simulator.API.DeviceManagement\IoT.Simulator.API.DeviceManagement.API\Dockerfile ..\sources\IoT.Simulator
 
 #2-Run a container based on the just created image
-docker run -ti --name iothubapi -P -p 5001:443 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORT=5001 -e ASPNETCORE_Kestrel__Certificates__Default__Path [path] -e ASPNETCORE_Kestrel__Certificates__Default__Password [password] iothubsdk-api
+echo "IoT HUB SDK REST API-Container-Creation"
+docker run -ti --name iothubapi -P -p 5001:443 -e ASPNETCORE_URLS="https://+" -e ASPNETCORE_HTTPS_PORT=5001  iothubsdk-api
+
+echo "IoT HUB SDK REST API-Image and container created"
