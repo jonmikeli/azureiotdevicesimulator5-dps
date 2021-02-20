0a1b742f91c4cb4e6cc68201e6b5688ccec35e79
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-02-21 00:34:37 +0100

    Command parameters

---
+++ sources/IoT.Simulator/IoT.Simulator/Settings/DPS/Commands/DPSCAX509CommandParameters.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+using Microsoft.Azure.Devices.Client;
+
+namespace IoT.Simulator.Settings.DPS
+{
+    /// <summary>
+    /// Parameters for the application
+    /// </summary>
+    internal class DPSCAX509CommandParameters : DPSCommandParametersBase
+    {
+        [Option(
+           'c',
+           "DeviceCertificate",
+           Required = true,
+           HelpText = "The device X509 certificate relative path (leaf) for DPS group enrollment.")]
+        public string DeviceCertificatePath { get; set; }
+    }
+}
