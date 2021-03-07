c7c58f3143696bda7b6f5cec87e65c50375fad1f
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-03-07 01:09:38 +0100

    commands updates

--- containers/CreateDevices.sh
+++ containers/CreateDevices.sh
@@ -6,8 +6,9 @@ do
   deviceName='autodevice'$i
 
   echo "Creating the device: $deviceName"
-  docker run -ti --rm --name $deviceName -d -e PROVISIONING_REGISTRATION_ID=$deviceName -e ENVIRONMENT=Development --network="host" iotsimulator-dps
+  docker run -ti --rm --name $deviceName -d -e PROVISIONING_REGISTRATION_ID=$deviceName -e ENVIRONMENT=Development -e DPS_SECURITY_TYPE="X509CA" -e DEVICE_CERTIFICATE_PATH="" -e DEVICE_CERTIFICATE_PASSWORD="" --network="host" iotsimulator-dps
+  docker run -ti --rm --name $deviceName -d -e PROVISIONING_REGISTRATION_ID=$deviceName -e ENVIRONMENT=Development -e DPS_SECURITY_TYPE="SymmetricKey" -e PRIMARY_SYMMETRIC_KEY="" --network="host" iotsimulator-dps
   echo "Device created: $deviceName"
   
-  let "i+=1" 
+  let "i+=2" 
 done
