aad978505e353f448b651220266ca6d43beec955
Jon Mikel Inza <22737122+jonmikeli@users.noreply.github.com>
2021-03-10 21:09:23 +0100

    error fixes

--- containers/CreateDevices.sh
+++ containers/CreateDevices.sh
@@ -12,14 +12,14 @@ symmetricKey="ii3VEjzWZpxhPlWT85O8sg/hZvqk2sNPHPDsP+M9v73BKs9NQHky+Tvg/IFNu1QEWq
 while [ $i -lt 20 ]
 do
   deviceName1='autodevice'$i
-  deviceName2='autodevice'$i+1
+  deviceName2='autodevice'$((i+1))
 
   echo "Creating the device: $deviceName1"
-  docker run -ti --rm --name $deviceName1 -d -e PROVISIONING_REGISTRATION_ID=$deviceName -e ENVIRONMENT=Development -e DPS_SECURITY_TYPE="X509CA" -e TRANSPORT_TYPE=$transportType -e DPS_IDSCOPE=$dpsIDScope -e DEVICE_CERTIFICATE_PATH=$x509CertificatePath -e DEVICE_CERTIFICATE_PASSWORD=$x509certificatePassword --network="host" iotsimulator-dps
+  docker run -ti --rm --name $deviceName1 -d -e PROVISIONING_REGISTRATION_ID=$deviceName1 -e ENVIRONMENT=Development -e DPS_SECURITY_TYPE="X509CA" -e TRANSPORT_TYPE=$transportType -e DPS_IDSCOPE=$dpsIDScope -e DEVICE_CERTIFICATE_PATH=$x509CertificatePath -e DEVICE_CERTIFICATE_PASSWORD=$x509certificatePassword --network="host" iotsimulator-dps
   echo "Device created: $deviceName1"
 
   echo "Creating the device: $deviceName2"
-  docker run -ti --rm --name $deviceName2 -d -e PROVISIONING_REGISTRATION_ID=$deviceName -e ENVIRONMENT=Development -e DPS_SECURITY_TYPE="SymmetricKey" -e TRANSPORT_TYPE=$transportType -e DPS_IDSCOPE=$dpsIDScope -e PRIMARY_SYMMETRIC_KEY=$symmetricKey --network="host" iotsimulator-dps
+  docker run -ti --rm --name $deviceName2 -d -e PROVISIONING_REGISTRATION_ID=$deviceName2 -e ENVIRONMENT=Development -e DPS_SECURITY_TYPE="SymmetricKey" -e TRANSPORT_TYPE=$transportType -e DPS_IDSCOPE=$dpsIDScope -e PRIMARY_SYMMETRIC_KEY=$symmetricKey --network="host" iotsimulator-dps
   echo "Device created: $deviceName2"
   
   let "i+=2" 
